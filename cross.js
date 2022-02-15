5b41114540db33a490eedd8e7ea0927b376e900b
unknown <ramiropen.work@gmail.com>
2022-02-15 15:49:30 -0800

    No real progress in class today, a lot of trial and error. Trying to workourk out what direction i wanna go in the css section.

--- cross.js
+++ cross.js
@@ -10,4 +10,43 @@ function checkAnswer() {
     document.querySelector("input").value = "";
 }
 
+
 console.log(checkAnswer)
+
+
+
+// const map = [
+//     "WWWWWWWWWW",
+//     "WP       W",
+//     "WWWWW WW W",
+//     "W W   W  W",
+//     "W W WWWWWW",
+//     "W    W WFW",
+//     "WWWW   W W",
+//     "W    WWW W",
+//     "W WW W   W",
+//     "W  W   W W",
+//     "WWWWWWWWWW",
+//   ];
+  
+//   function buildMap(table) {
+//     for (let tr = 0; tr < table.length; tr++) {
+//       let rowDiv = document.createElement("div");
+//       rowDiv.classList.add("row");
+  
+//       for (let td = 0; td < table[tr].length; td++) {
+//         let cell = document.createElement("div");
+//         cell.classList.add("cell");
+  
+//         let cellType = table[tr][x];
+  
+//         if (cellType === "W") {
+//           cell.classList.add("wall");
+//         }
+//         rowDiv.append(cell);
+//       }
+  
+//       let mapDiv = document.querySelector(".map");
+//       mapDiv.append(rowDiv);
+//     }
+//   }
